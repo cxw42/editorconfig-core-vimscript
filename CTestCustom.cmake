8ad26d01b06d9ab50aeb1c6c346c4a2a83b09d6f
Chris White <cxwembedded@gmail.com>
2019-04-16 07:35:00 -0400

    Run UTF-8 tests on Appveyor Ubuntu

--- CTestCustom.cmake
+++ CTestCustom.cmake
@@ -1,10 +1,5 @@
-# CTestCustom.cmake: Skip some tests
+# CTestCustom.cmake: Skip UTF-8 tests
 # Part of editorconfig-core-vimscript
-
-## See editorconfig/editorconfig#371 - I think these should succeed, but the
-## current test suite disagrees with me.
-#set(CTEST_CUSTOM_TESTS_IGNORE ${CTEST_CUSTOM_TESTS_IGNORE} g_braces_numeric_range8)
-#set(CTEST_CUSTOM_TESTS_IGNORE ${CTEST_CUSTOM_TESTS_IGNORE} braces_numeric_range8)
 
 # I need more vimscript Unicode regex skills before I can make these pass.
 set(CTEST_CUSTOM_TESTS_IGNORE ${CTEST_CUSTOM_TESTS_IGNORE} g_utf_8_char)
